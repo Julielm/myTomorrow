b214f34e431f0216eba558049a6ee652803328c6
Julielm <julielm.leclerc@hotmail.fr>
2015-05-31 12:27:53 +0200

    Addition of buttons : previous week and next week

--- src/myTomorrow/view/gui/Calendar.java
+++ src/myTomorrow/view/gui/Calendar.java
@@ -1,5 +1,6 @@
 package myTomorrow.view.gui;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
@@ -7,6 +8,9 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -37,6 +41,23 @@ public class Calendar extends JPanel
 		{
 		      e.printStackTrace();
 		}  
+		JButton previousWeek = new JButton();
+		Icon previous = new ImageIcon("PreviousWeek.png");
+		previousWeek.setIcon(previous);
+		previousWeek.setBounds(1,8,28,30);
+		previousWeek.setBorder(BorderFactory.createEmptyBorder());
+		previousWeek.setBackground(Color.WHITE);
+		this.add(previousWeek);
+		
+		JButton nextWeek = new JButton();
+		Icon next = new ImageIcon("NextWeek.png");
+		nextWeek.setIcon(next);
+		nextWeek.setBounds(28,8,28,30);
+		nextWeek.setBorder(BorderFactory.createEmptyBorder());
+		nextWeek.setBackground(Color.WHITE);
+		this.add(nextWeek);
+		
+		
 		JButton event = new GraphicalEvent(new ScheduledEvent(new TimeSlot(new DateTime(2015,05,15,8,15), new DateTime(2015,05,15,9,15))));
 		this.add(event);
 //		for (int day=0; day <7; day++) {
