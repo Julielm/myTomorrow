4c6b86ac01b5bd444b30b8bc13cadf17640ef101
Julielm <julielm.leclerc@hotmail.fr>
2015-06-07 16:25:12 +0200

    Modifications

--- src/myTomorrow/view/gui/MainWindow.java
+++ src/myTomorrow/view/gui/MainWindow.java
@@ -3,8 +3,6 @@ package myTomorrow.view.gui;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.util.List;
 
 import javax.swing.BorderFactory;
